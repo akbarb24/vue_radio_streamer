1adae9070f50d4d645e6e39ae6a8d3c9d1d33360
Akbar Binaji <akbar.binaji@gmail.com>
2019-02-20 18:39:29 +0700

    create home layout and button

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,15 +1,36 @@
 <template>
-  <div class="container is-fluid">
-    <div class="notification">
-      This container is
-      <strong>fluid</strong>: it will have a 32px gap on either side, on any
-      viewport size.
+  <div class="container">
+    <div class="columns is-centered">
+      <div class="column is-4">
+        <div class="card">
+          <div class="card-image">
+            <figure class="image">
+              <img alt="Disc" src="@/assets/disc.jpg">
+            </figure>
+          </div>
+          <div class="card-content is-flex">
+            <figure class="image is-48x48">
+              <play-button></play-button>
+            </figure>
+            <figure class="image is-48x48">
+              <pause-button></pause-button>
+            </figure>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 <script>
+import PlayButton from "./PlayButton.vue";
+import PauseButton from "./PauseButton.vue";
+
 export default {
-  name: "HomePage"
+  name: "HomePage",
+  components: {
+    "play-button": PlayButton,
+    "pause-button": PauseButton
+  }
 };
 </script>
 
