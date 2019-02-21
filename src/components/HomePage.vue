bf85697391adf6a50ed664a775aa74f7ab628f50
Akbar Binaji <akbar.binaji@gmail.com>
2019-02-21 20:49:14 +0700

    streamer worked cannot pause

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -8,13 +8,25 @@
               <img alt="Disc" src="@/assets/disc.jpg">
             </figure>
           </div>
-          <div class="card-content is-flex">
-            <figure class="image is-48x48">
-              <play-button></play-button>
-            </figure>
-            <figure class="image is-48x48">
-              <pause-button></pause-button>
-            </figure>
+          <div class="card-content">
+            <a
+              class="button is-large is-rounded is-white"
+              :disabled="isPlaying"
+              @click="playing(isPlaying)"
+            >
+              <span class="icon is-large has-text-warning">
+                <i class="far fa-2x fa-play-circle"></i>
+              </span>
+            </a>
+            <a
+              class="button is-large is-rounded is-white"
+              :disabled="!isPlaying"
+              @click="playing(isPlaying)"
+            >
+              <span class="icon is-large has-text-warning">
+                <i class="far fa-2x fa-pause-circle"></i>
+              </span>
+            </a>
           </div>
         </div>
       </div>
@@ -22,14 +34,28 @@
   </div>
 </template>
 <script>
-import PlayButton from "./PlayButton.vue";
-import PauseButton from "./PauseButton.vue";
-
 export default {
   name: "HomePage",
-  components: {
-    "play-button": PlayButton,
-    "pause-button": PauseButton
+  data() {
+    return {
+      isPlaying: false
+    };
+  },
+  methods: {
+    playing: function(play) {
+      this.isPlaying = !play;
+
+      const radio_url = "http://103.226.246.58:80/masima-pramborsjakarta";
+      var audio = new Audio(radio_url);
+
+      if (this.isPlaying) {
+        console.log("Playing...")
+        audio.play();
+      } else {
+        console.log("Pause");
+        // audio.pause();
+      }
+    }
   }
 };
 </script>
